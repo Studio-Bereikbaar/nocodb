e183680a2e572c9b461dbf25de664f0a450ce4c1
Fendy Heryanto <fendy@nocodb.com>
2026-03-11 05:04:03 +0000

    v3 meta comment

--- packages/nocodb-sdk/src/lib/payment/index.ts
+++ packages/nocodb-sdk/src/lib/payment/index.ts
@@ -92,6 +92,7 @@ export enum PlanFeatureTypes {
   FEATURE_AI_CHAT = 'feature_ai_chat',
   FEATURE_DOCS_INLINE_COMMENTS = 'feature_docs_inline_comments',
   FEATURE_DOCS_EXPORT_PDF = 'feature_docs_export_pdf',
+  FEATURE_API_COMMENT_V3 = 'feature_api_comment_v3',
 }
 
 export enum PlanTitles {
@@ -339,6 +340,7 @@ export const PlanFeatureUpgradeMessages: Record<PlanFeatureTypes, string> = {
     'to use inline comments in documents.',
   [PlanFeatureTypes.FEATURE_DOCS_EXPORT_PDF]:
     'to export documents as PDF.',
+  [PlanFeatureTypes.FEATURE_API_COMMENT_V3]: 'to use comment api.',
 };
 
 export const getUpgradeMessage = (
